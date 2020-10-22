0c21f70a361926cfddf0040e5d6fb52b84cc45a6
陈炎 <chenyan@chenyandeMacBook-Pro.local>
2020-10-22 14:43:39 +0800

    scroll

---
+++ topbar.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="searchBar" class="searchBar">
+        <ul :class="searchBarFixed == true ? 'isFixed' : ''" >
+            <li>首页</li>
+            <li>更多</li>
+            <li>购物</li>
+            <li>我的</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    mounted(){
+        window.addEventListener('scroll', this.handleScroll)
+    },
+    data(){
+        return{
+            searchBarFixed:false,
+        }
+    },
+  
+    methods:{
+        handleScroll(){
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop ||
+            document.body.scrollTop;
+            // console.log(scrollTop)
+            var offsetTop =document.querySelector('#searchBar').offsetTop;
+            if(scrollTop>offsetTop){
+                this.searchBarFixed = true
+            }else{
+                this.searchBarFixed = false
+            }
+        }  
+    },
+      destroyed(){
+        window.removeEventListener('scroll', this.handleScroll)
+    },
+}
+</script>
+
+<style scoped>
+    .searchBar .isFixed{
+        position:fixed;
+        background-color: gray;
+        top: 0;
+        z-index:999;
+    }
+    ul{
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+        display: flex;
+        border-bottom: 1px solid #ddd;
+    }
+    ul li{
+        font-size:0.8rem;
+        text-align:center;
+        flex:1;
+    }
+    
+</style>
+
